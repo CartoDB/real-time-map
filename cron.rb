6d73a70a02daade04278768047ff3ec213c3ed37
Javier Arce <javierarce@gmail.com>
2012-10-24 17:42:58 +0200

    Check every 30000ms

--- cron.rb
+++ cron.rb
@@ -7,9 +7,10 @@ while(true)
   system("curl -d \"q=UPDATE states_results SET gov_result = #{i}&api_key=cb07c48abf5eda40ab2b66818f8b1733f4d1ce0a\" http://viz2.cartodb.com/api/v2/sql")
   puts "#{i}"
 
-  i = i + 1
 
-  i = 0 if i == 7
+  i = -1 if i == 7
+
+  i = i + 1
 
   sleep 5
 end
