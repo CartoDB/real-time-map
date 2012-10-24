e1c738a7546ff2377728a8812e9eb32ad0415ca3
Javier Arce <javierarce@gmail.com>
2012-10-24 17:29:03 +0200

    Adds colored version

---
+++ cron.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+i = 0
+
+while(true) 
+
+  system("curl -d \"q=UPDATE states_results SET gov_result = #{i}&api_key=cb07c48abf5eda40ab2b66818f8b1733f4d1ce0a\" http://viz2.cartodb.com/api/v2/sql")
+  puts "#{i}"
+
+  i = i + 1
+
+  i = 0 if i > 7
+  sleep 5
+end
